72b0613e9e1915a156a90f5ef335faf621227f4b
Aszparuh <puhabg@live.com>
2019-01-10 15:54:46 +0200

    Starts building timer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { interval } from 'rxjs';
+import { takeWhile, tap } from 'rxjs/operators';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +11,39 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'timers';
+
+  max     = 1;
+  current = 0;
+
+  /// Start the timer
+  start() {
+        interval(100)
+        .pipe(takeWhile(_ => !this.isFinished ))
+        .pipe(tap(i => this.current += 0.1))
+        .subscribe()
+  }
+
+   /// finish timer
+  finish() {
+    this.current = this.max;
+  }
+
+  /// reset timer
+  reset() {
+    this.current = 0;
+  }
+
+  /// Getters to prevent NaN errors
+
+  get maxVal() {
+    return isNaN(this.max) || this.max < 0.1 ? 0.1 : this.max;
+  }
+
+  get currentVal() {
+    return isNaN(this.current) || this.current < 0 ? 0 : this.current;
+  }
+
+  get isFinished() {
+    return this.currentVal >= this.maxVal;
+  }
 }
